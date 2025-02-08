e7964f56e0b6d30297e5c76faa9e03ca9657b52c
Леонид Половков <leonid.spb2015@yandex.ru>
2025-02-08 21:53:12 +0300

    Корректировка главной страницы

--- js/search-focus-input.js
+++ js/search-focus-input.js
@@ -1,4 +1,4 @@
-let fileInput = document.getElementById("search");
+let fileInput = document.querySelector(".search");
 let isFocused = false;
 let searchCancel = document.querySelector(".search__cancel");
 let searchingBlock = document.querySelector(".searching-block");
@@ -6,9 +6,11 @@ let searchingBlock = document.querySelector(".searching-block");
 
 
 searchingBlock.addEventListener("focus", () => {  
-    fileInput.classList.toggle = "search-area-compact";
+    fileInput.classList.toggle("search-area-compact");
     searchCancel.style.display = "flex";
 });
+
+
 
 
 
