365434abbf0b1420f1a6e724a80069a143131220
Леонид Половков <leonid.spb2015@yandex.ru>
2025-02-08 13:58:13 +0300

    Верстка страницы выбор участников. Сделал радиокнопки

---
+++ js/serach-focus-input.js
@@ -0,0 +1,17 @@
+let fileInput = document.getElementById("search");
+let isFocused = false;
+let searchCancel = document.querySelector(".search__cancel");
+let searchingBlock = document.querySelector(".searching-block");
+
+
+
+searchingBlock.addEventListener("focus", () => {  
+    fileInput.classList.toggle = "search-area-compact";
+    searchCancel.style.display = "flex";
+});
+
+
+
+
+
+
