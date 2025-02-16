d0d3355c239dcbdfc67c085491eec031ec80f432
Леонид Половков <leonid.spb2015@yandex.ru>
2025-02-16 21:55:42 +0300

    Скорректировал смену цвета у footer при смены светлой темы на темную

--- js/change-theme.js
+++ js/change-theme.js
@@ -6,21 +6,22 @@ let swiperSlideActive = document.querySelector(".swiper-slide-active");
 let contentEvent = document.querySelector(".content-event");
 let memberName = document.querySelectorAll(".member-name");
 let h3 = document.getElementsByTagName("h3");
-let footer = document.getElementById("footer");
 let input = document.getElementById("input[type='checkbox]");
 let eventColor = document.querySelector(".event-color");
 let span = document.getElementsByTagName("span");
 let darkTheme = document.querySelector(".dark-theme");
 let recordOption = document.querySelector(".record-option");
 let sectionMembersEvent = document.querySelectorAll(".section-members-event");
+let sectionAccountItem = document.querySelector(".settings__accounts_item");
+let footer = document.getElementById("footer");
 
 toggle.addEventListener("click", function() {
-  mainSectionBody.classList.toggle("dark-theme");
-  headlineApp.classList.toggle("light-them-color-text");
-  h3.classList.toggle("dark-theme-color-text");
-  recordOption.classList.toggle(".record-option-dark");
-
-  for (let i = 0; i <= memberName.length; i++) {
-    h3[i].style.color = "white";
+  if (!darkTheme) {
+    mainSectionBody.classList.toggle("dark-theme");
+    headlineApp.classList.toggle("light-them-color-text");
+    recordOption.classList.toggle("record-option-dark");
+    footer.classList.toggle("dark-theme");
+    sectionAccountItem.classList.toggle("dark-text");
+    swiperSlideActive.classList.toggle("dark-text");
   }
 });
