26e921625721c9d3326f5877714d9f3ef799b20e
Леонид Половков <leonid.spb2015@yandex.ru>
2025-02-18 21:43:42 +0300

    Добавил функционал скрытия блоков начала и конца события при включении переключателя на целый день

--- js/change-theme.js
+++ js/change-theme.js
@@ -10,8 +10,10 @@ let h3 = document.getElementsByTagName("h3");
 let nameHeadline = document.querySelectorAll(".name");
 let input = document.getElementById("input[type='checkbox]");
 let eventColor = document.querySelector(".event-color");
+let headline = document.querySelector(".headline");
 let span = document.getElementsByTagName("span");
-let darkTheme = document.querySelector(".dark-theme");
+let dayWeek = document.querySelector(".day-week");
+let lightTheme = document.querySelector(".light-theme");
 let advertisementText = document.querySelector(".advertisement-text");
 let recordOption = document.querySelector(".record-option");
 let sectionMembersEvent = document.querySelectorAll(".section-members-event");
@@ -19,17 +21,21 @@ let sectionAccountItem = document.querySelector(".settings__accounts_item");
 let footer = document.getElementById("footer");
 
 toggle.addEventListener("click", function() {
-  if (!darkTheme) {
+  if (!lightTheme) {
     mainSectionBody.classList.toggle("dark-theme");
     referenceBody.classList.toggle("dark-theme");
     headlineApp.classList.toggle("light-them-color-text");
+    headline.classList.toggle("dark-text");
     recordOption.classList.toggle("record-option-dark");
     footer.classList.toggle("dark-theme");
     sectionAccountItem.classList.toggle("dark-text");
     swiperSlideActive.classList.toggle("dark-text");
-    h3.classList.toggle("Theme");
     nameHeadline.classList.toggle("name-dark");
+    dayWeek.classList.toggle("day-week-light");
     eventColor.classList.toggle("color-event-dark-active");
     advertisementText.style.color = "white";
+    nameEvent.style.color = "black";
+  } else {
+    headline.classList.toggle("light-them-color-text");
   }
 });
---
+++ js/switcher-event.js
@@ -0,0 +1,27 @@
+let startEventBlock = document.querySelector(".start-event-block");
+let endEventBlock = document.querySelector(".end-event-block");
+let allDayBlockFirst = document.querySelector(".all-day-block:first-child");
+
+$(".switch-btn").click(function() {
+  $(this).toggleClass("switch-on");
+  if ($(this).hasClass("switch-on")) {
+    $(this).trigger("on.switch");
+    startEventBlock.style.display = "none";
+    endEventBlock.style.display = "none";
+    allDayBlockFirst.style.borderBottom = "none";
+  } else {
+    $(this).trigger("off.switch");
+    startEventBlock.style.display = "flex";
+    endEventBlock.style.display = "flex";
+     allDayBlockFirst.style.borderBottom = "flex";
+  }
+});
+$(".switch-btn-video-conf").click(function() {
+  $(this).toggleClass("switch-on-conf");
+  if ($(this).hasClass("switch-on-conf")) {
+    $(this).trigger("on.switch");
+  
+  } else {
+    $(this).trigger("off.switch");  
+  }
+});
